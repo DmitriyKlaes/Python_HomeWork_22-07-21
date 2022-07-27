c22e40e1b7c9992dde9a4f261a48944af774532f
Dmitriy_Klaes <89119584059@mail.ru>
2022-07-28 01:44:55 +0300

    task000 - сложение многочленов (пилю) 22-07-28

--- task000-sum_polinomial.py
+++ task000-sum_polinomial.py
@@ -1,60 +1,61 @@
 # Даны два файла, в каждом из которых находится запись многочлена. 
 # Задача - сформировать файл, содержащий сумму многочленов.
 
-# polinomial_1 = ''
-import re
-# with open('task000-polinomial_1.txt', 'r') as file_1:
-#     polinomial_1 = [i for i in reversed(file_1.read()) if i != ' ']
-# with open('task000-polinomial_2.txt', 'r') as file_2:
-#     polinomial_2 = [i for i in reversed(file_2.read()) if i != ' ']
-with open('task000-polinomial_1.txt', 'r') as file_1:
-    polinomial_1 = file_1.read().split()
-with open('task000-polinomial_2.txt', 'r') as file_2:
-    polinomial_2 = file_2.read().split()
+def import_file(name):
+    with open(name, 'r') as file_1:
+        return [i for i in file_1.read().split()]
 
-print(polinomial_1)
-print(polinomial_2)
-s = "text text : one two three"
-print(s[s.find(":")])
-# for i in range(len(polinomial_2)):
-#     if polinomial_2[i] == '-':
-#         polinomial_2[i - 1] = int(polinomial_2[i - 1]) * -1
-    
-# print(polinomial_1)
-# print(polinomial_2)
-    
+def coeff_symbol(pol):
+    count = 0
+    for i in pol:
+        if i == '-' or i == '+':
+            pol[count + 1] = pol[count] + pol[count + 1]
+            pol.pop(count)
+        count += 1
 
-# dic = {i: x for i, x in zip(reversed(polinomial_1), polinomial_2)}
-# print(dic)
+def get_dict(pol):
+    for i in range(len(pol)):
+        pol[i] = list(reversed(pol[i].replace('^', '').partition("x")))
+        if pol[i][1] == 'x' and pol[i][0] == '':
+            pol[i][0] = 1
+        if pol[i][1] == '' and pol[i][0] == '':
+            pol[i][0] = 0
+        pol[i].pop(1)
+        pol[i] = list(map(int, pol[i]))
+    return dict(pol)
 
-str1 = "Python"
-dic = {i: x for i, x in enumerate(reversed(str1), 1)}
-print(dic)
+def summ_pol(dict1, dict2):
+    max_dict = {}
+    min_dict = {}
+    result = {}
+    if len(dict1) > len(dict2):
+        max_dict, min_dict = dict1, dict2
+    else:
+        max_dict, min_dict = dict2, dict1   
+    for i in range(len(max_dict)+(len(max_dict)-len(min_dict)) -1, -1, -1):
+        if i in min_dict.keys() and i not in max_dict.keys():
+            result[i] = min_dict[i]
+        else:
+            result[i] = (max_dict[i] if i in max_dict else 0) + (min_dict[i] if i in min_dict else 0)
+    return result
+
+def simple_polynomial(dict):
+    result = ' '
+    pow = len(dict) - 1
+    while pow >= 0:
+        if pow > 1: result = result + str(dict[pow] if pow in dict else 0) + 'x^' + str(pow)
+        if pow == 1: result = result + str(dict[pow] if pow in dict else 0) + 'x'
+        if pow == 0: result = result + str(dict[pow] if pow in dict else 0)
+        if pow > 0: result = result + ' + '
+        pow -= 1
+    return result.replace(' 1x', ' x').replace(' 0x', ' x').replace('+ 0', '')
 
 
-
-# print(file_1.writelines)
-
-# dictionary = {}
-# dictionary = \
-#     {
-#         'up': '↑',
-#         'left': '←',
-#         'down': '↓',
-#         'right': '→'
-#     }
-
-# for item in dictionary:  # for (k,v) in dictionary.items():
-#     print('{}: {}'.format(item, dictionary[item]))
-    
-# print(dictionary)
-
-# for k in dictionary.keys():
-#     print(k)
-
-# for k in dictionary.values():
-#     print(k)
-
-# for k in dictionary:
-#     print(dictionary[k])
-
+file_1, file_2 = import_file('task000-polinomial_1.txt'), import_file('task000-polinomial_2.txt')
+coeff_symbol(file_1)
+coeff_symbol(file_2) 
+dict_1 = get_dict(file_1)
+dict_2 = get_dict(file_2)
+result_dict = summ_pol(dict_1, dict_2)
+print(f'({simple_polynomial(dict_1)}) + ({simple_polynomial(dict_2)})', end=' =')
+print(simple_polynomial(result_dict))
